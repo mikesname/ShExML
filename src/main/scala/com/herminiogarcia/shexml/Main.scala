fa4d2f8dbc2c7b6ff7904abd38984538c24222f0
Herminio García González <herminiogg@hotmail.com>
2025-10-01 15:20:17 +0200

    Version 0.6.1

--- src/main/scala/com/herminiogarcia/shexml/Main.scala
+++ src/main/scala/com/herminiogarcia/shexml/Main.scala
@@ -20,7 +20,7 @@ object Main {
   }
 }
 
-@Command(name = "ShExML", version = Array("v0.6.0"),
+@Command(name = "ShExML", version = Array("v0.6.1"),
   mixinStandardHelpOptions = true,
   sortOptions = false,
   description = Array("Map and merge heterogeneous data sources with a Shape Expressions based syntax"))
